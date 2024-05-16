5f26577075d527fa5a6bfe05842b154671346769
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-16 05:42:54 +0500

    add fail mode Di

---
+++ Solid/DI/Fail/Database.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.DI.Fail
+{
+    public class Database
+    {
+        public void SaveData(string data)
+        {
+            // save any data
+        }
+    }
+}
---
+++ Solid/DI/Fail/UserManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.DI.Fail
+{
+    public class UserManager
+    {
+        private readonly Database _database;
+
+        public UserManager()
+        {
+            _database = new Database();
+        }
+
+        public void AddUser(string userData)
+        {
+            _database.SaveData(userData); 
+        }
+    }
+}
