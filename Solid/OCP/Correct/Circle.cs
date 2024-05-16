a07d9a53b2dd6446a6ead3476e896c6ecaf25784
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-16 05:16:18 +0500

    add correct version OCP

---
+++ Solid/OCP/Correct/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.OCP.Correct
+{
+    public class Circle : IShape
+    {
+        public double radius { get; set; }
+
+        public double getArea()
+        {
+            return radius * radius * Math.PI;
+        }
+    }
+}
---
+++ Solid/OCP/Correct/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.OCP.Correct
+{
+    public class Rectangle : IShape
+    {
+        public double length { get; set; }
+        public double height { get; set; }
+        public double getArea()
+        {
+            return length * height;
+        }
+    }
+}
