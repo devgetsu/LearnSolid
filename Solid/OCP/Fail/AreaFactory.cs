582fa6d0e1b349e2085958a21abb614ac0fe2eec
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-16 05:12:15 +0500

    add OCP fail

---
+++ Solid/OCP/Fail/AreaFactory.cs
@@ -0,0 +1,18 @@
+﻿using Solid.OCP.Fail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.OCP.Fail
+{
+
+    public class AreaFactory
+    {
+        public double calculateArea(List<Object> shapes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Solid/OCP/Fail/Rectangle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.OCP.Fail
+{
+    public class Rectangle
+    {
+        public double length {  get; set; }
+        public double height { get; set; }
+    }
+}
