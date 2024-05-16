63fdaef53cf0bd22532b4d8d1cec1abc4653c1c6
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-16 05:05:42 +0500

    add srp fully

---
+++ Solid/SRP/Correct/AuthService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.SRP.Correct
+{
+    public class AuthService
+    {
+        public void SignUp()
+        {
+            // bu funksiya signUp qilish uchun
+        }
+
+        public void SignOut()
+        {
+            // bu funksiya signOut qilish uchun
+        }
+    }
+}
