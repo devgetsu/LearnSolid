7a1bad7b8bd6be156765d877b59a254deb7a49d0
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-05-16 05:04:22 +0500

    add fail class and correct class

---
+++ Solid/SRP/Fail/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.SRP.Fail
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public void SignUp()
+        {
+            // bu funksiya signUp qilish uchun
+        }
+
+        public void SignOut()
+        {
+            // bu funksiya signOut qilish uchun
+        }
+    }
+}
